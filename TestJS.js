3b476fecc2089d5f88d162967f920253fbed966c
Vincent Boyer <boyer.vincent@gmail.com>
2022-06-15 16:41:16 +0200

    Update TestJS.js
    Boom regression

--- TestJS.js
+++ TestJS.js
@@ -1,1 +1,3 @@
 var mongo_uri = "mongodb+srv://testuser:hub25aoeu@gg-is3.mongodb.net/test?retryWrites=true&w=majority";
+
+var mongo_uri = "mongodb+srv://testuser:hub25aoeu@gg-is3.mongodb.net/test?retryWrites=true&w=majority";
