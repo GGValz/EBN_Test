c3e13056f6628478c3562f0e0c49fb0639b67402
Vincent Boyer <boyer.vincent@gmail.com>
2022-06-15 17:09:33 +0200

    Update TestJS.js
    AWS

--- TestJS.js
+++ TestJS.js
@@ -1,3 +1,7 @@
 var mongo_uri = "mongodb+srv://testuser:hub25aoeu@gg-is3.mongodb.net/test?retryWrites=true&w=majority";
 
 var mongo_uri = "mongodb+srv://testuser:hub25aoeu@gg-is3.mongodb.net/test?retryWrites=true&w=majority";
+
+
+database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ9", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkje")
+ No newline at end of file
