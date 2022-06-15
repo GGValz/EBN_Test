0fad8bbcaf17dc16f61a9cd7c965cbef44b5d7d5
Vincent Boyer <boyer.vincent@gmail.com>
2022-06-15 17:10:42 +0200

    Update TestJS.js
    AWS2

--- TestJS.js
+++ TestJS.js
@@ -1,7 +1,3 @@
-var mongo_uri = "mongodb+srv://testuser:hub25aoeu@gg-is3.mongodb.net/test?retryWrites=true&w=majority";
 
-var mongo_uri = "mongodb+srv://testuser:hub25aoeu@gg-is3.mongodb.net/test?retryWrites=true&w=majority";
-
-
-database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ9", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkje")
+database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ2", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkje")
  No newline at end of file
