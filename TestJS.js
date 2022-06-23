5db5e2e5328e644029b9377f0217f82b1caac2be
Vincent Boyer <boyer.vincent@gmail.com>
2022-06-23 11:10:22 +0200

    Update TestJS.js

--- TestJS.js
+++ TestJS.js
@@ -1,3 +1,7 @@
 
 database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ0", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkje")
  No newline at end of file
+ 
+ 
+ database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ0", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkje")
+ No newline at end of file
